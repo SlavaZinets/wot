0089998347edd2fc939d2b125bba3202f068904a
SlavaZinets <slavazinets4@gmail.com>
2022-09-17 23:16:14 +0300

    home page created

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-fetch(`https://api.worldoftanks.ru/wot/account/list/?search=WildPants_2&fields=-&application_id=88de5ca6c8e18fa6a41bb502d6cfb95a`)
-.then ( (data) => {
-    console.log(data);
-})
-.catch(error => window.alert("лох"));
+// fetch(`https://api.worldoftanks.ru/wot/tanks/stats/?application_id=88de5ca6c8e18fa6a41bb502d6cfb95a&account_id=132479554`, {
+//     method: 'GET',
+//     mode: "cors",
+// })
+// .then ( response => response.json())
+// .then ( (dataServer) => {
+//     // data();
+//     console.log(dataServer.data[132479554]);
+// })
+
+const body = document.querySelector('body');
+
+const searchNameButton = document.getElementById('searchNameButton');
+const searchIDButton = document.getElementById('searchIDButton');
+
+
+searchNameButton.addEventListener('click', searchThisName);
+searchIDButton.addEventListener('click', searchThisID)
+
+
+function searchThisName () {
+    const inputName = document.getElementById('inputNameValue').value;
+        console.log(inputName);
+}
+
+function searchThisID () {
+    const inputID = document.getElementById('inputIDValue').value;
+    console.log(inputID);
+}
